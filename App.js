720b7f9edd032d0d43dcc3fd22f9a370a33183fd
stanislaw-rakowski <stasiu.rakowski@gmail.com>
2022-01-07 20:00:42 +0100

    layout improvements

--- App.js
+++ App.js
@@ -2,11 +2,26 @@ const processCountSlider = document.querySelector('.process-count-range');
 const countOutput = document.querySelector('.process-count-output');
 const compareCheckbox = document.querySelector('.compare-checkbox');
 const computeBtn = document.querySelector('.compute-btn');
+const inputsDiv = document.querySelector('.inputs-div');
 
 countOutput.textContent = processCountSlider.value;
 
 processCountSlider.oninput = function() {
     countOutput.textContent = this.value;
+
+    inputsDiv.innerHTML = '';
+    let el;
+
+    for(let i = 1; i <= this.value; i++) {
+      el = `
+      <div class="input-row">
+        <p>P${i}</p>
+        <input class="duration-input duration-input-P${i}" type="number">
+        <input class="arrival-input arrival-input-P${i}" type="number">
+      </div>`;
+
+      inputsDiv.innerHTML += el;
+    }
 }
 
 
