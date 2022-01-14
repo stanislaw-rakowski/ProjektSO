6a7dcc2bca0ffb579f66e8332240f59b30820f78
stanislaw-rakowski <stasiu.rakowski@gmail.com>
2022-01-14 01:50:15 +0100

    added charts and test summary

--- App.js
+++ App.js
@@ -94,11 +94,360 @@ window.onclick = function(event) {
   }
 }
 
+const getRandomNumber = (max) => {
+  return Math.floor(Math.random() * max);
+}
+
 const testHandler = () => {
   testModal.style.display = "flex";
 
+  testProcesses();
+  testPages();
+  
 }
 
+const testPages = () => {
+  let testPages = [];
+
+  const xValues = [];
+
+  const FIFOresults = [];
+  const LRUresults = [];
+
+  for(let i = 0; i < 100; i++) {
+    for(let j = 0; j < 10; j++) {
+      testPages.push(getRandomNumber(40));
+    }
+
+    let capacity = getRandomNumber(7) + 1;
+
+    FIFOresults.push(calculateFIFO(testPages, capacity));
+    LRUresults.push(calculateLRU(testPages, capacity));
+
+    testPages = [];
+
+    xValues.push(i);
+  }
+
+  new Chart("myChartPages", {
+    type: "line",
+    data: {
+      labels: xValues,
+      datasets: [{
+        data: FIFOresults,
+        borderColor: "red",
+        fill: false
+      },{
+        data: LRUresults,
+        borderColor: "blue",
+        fill: false
+      }]
+    },
+    options: {
+      legend: {display: false},
+      scales: {
+        yAxes: [{
+          display: true,
+          ticks: {
+            suggestedMax: 12,
+            suggestedMin: 5 
+          }
+        }]
+      }
+    }
+  });
+
+  const resultsList = document.querySelector('.pages-test-results-ul');
+  resultsList.innerHTML = '';
+
+  for(let i = 0; i < 100; i++) {
+    resultsList.innerHTML += `
+    <li>
+      <p>P${i + 1}</p>
+      <p style="background-color: red;">${FIFOresults[i]}</p>
+      <p style="background-color: blue;">${LRUresults[i]}</p>
+    </li>
+  `;
+  }
+} 
+
+const testProcesses = () => {
+  let processesTestArrivals = [];
+  let processesTestDurations = [];
+
+  const xValues = [];
+
+  const WTtestResultsFCFS = [];
+  const TATtestResultsFCFS = [];
+  const WTtestResultsRR = [];
+  const TATtestResultsRR = [];
+
+  for(let i = 0; i < 100; i++) {
+    for(let j = 0; j < 10; j++) {
+      processesTestArrivals.push(getRandomNumber(20));
+      processesTestDurations.push(getRandomNumber(20) + 1);
+    }
+
+    let wt;
+    let tat;
+
+    [wt, tat] = calculateFCFS(processesTestDurations, processesTestArrivals);
+    WTtestResultsFCFS.push(wt);
+    TATtestResultsFCFS.push(tat);
+
+    [wt, tat] = calculateRoundRobin(processesTestDurations, processesTestArrivals);
+    WTtestResultsRR.push(wt);
+    TATtestResultsRR.push(tat);
+
+    processesTestDurations = [];
+    processesTestArrivals = [];
+
+    xValues.push(i);
+  }
+
+  new Chart("myChart", {
+    type: "line",
+    data: {
+      labels: xValues,
+      datasets: [{
+        data: WTtestResultsFCFS,
+        borderColor: "red",
+        fill: false
+      },{
+        data: TATtestResultsFCFS,
+        borderColor: "green",
+        fill: false
+      },{
+        data: WTtestResultsRR,
+        borderColor: "blue",
+        fill: false
+      },{
+        data: TATtestResultsRR,
+        borderColor: "orange",
+        fill: false
+      }]
+    },
+    options: {
+      legend: {display: false}
+    }
+  });
+
+  const resultsList = document.querySelector('.processes-test-results');
+  resultsList.innerHTML = '';
+
+  for(let i = 0; i < 100; i++) {
+    resultsList.innerHTML += `
+    <li>
+      <p>P${i + 1}</p>
+      <p style="background-color: red;">${WTtestResultsFCFS[i]}</p>
+      <p style="background-color: green;">${TATtestResultsFCFS[i]}</p>
+      <p style="background-color: blue;">${WTtestResultsRR[i]}</p>
+      <p style="background-color: orange;">${TATtestResultsRR[i]}</p>
+    </li>
+  `;
+  }
+}
+
+const calculateFCFS = (durationTimes, arrivalTimes) => {
+
+  const processesCount = arrivalTimes.length;
+  const waitingTimes = new Array(processesCount);
+  const turnAroundTimes = new Array(processesCount);
+
+  const serviceTime = new Array(processesCount);
+  serviceTime[0] = arrivalTimes[0];
+  waitingTimes[0] = 0;
+
+  for (let i = 1; i < processesCount; i++) {
+      let wastedTime = 0;
+
+      serviceTime[i] = serviceTime[i - 1] + durationTimes[i - 1];
+      waitingTimes[i] = serviceTime[i] - arrivalTimes[i];
+
+      if (waitingTimes[i] < 0) {
+          wastedTime = Math.abs(waitingTimes[i]);
+          waitingTimes[i] = 0;
+      }
+
+      serviceTime[i] = serviceTime[i] + wastedTime;
+  }
+
+  for (let i = 0; i < processesCount; i++) {
+      turnAroundTimes[i] = durationTimes[i] + waitingTimes[i];
+  }
+
+  let totalWaitingTime = 0;
+  let totalTurnAroundTime = 0;
+  let completionTime = 0;
+  const processesData = [];
+  
+  for (let i = 0 ; i < processesCount; i++) {
+      totalWaitingTime += waitingTimes[i];
+      totalTurnAroundTime += turnAroundTimes[i];
+      
+      completionTime = turnAroundTimes[i] + arrivalTimes[i];
+      
+      processesData.push({
+          number: i + 1,
+          duration: durationTimes[i],
+          arrivalTime: arrivalTimes[i],
+          waitingTime: waitingTimes[i],
+          turnAroundTime: turnAroundTimes[i],
+          timeWhenCompleted: completionTime
+      });
+  }
+  
+  let averageWaitingTime = Math.round((totalWaitingTime / processesCount) * 100) / 100;
+  let averageTurnAroundTime = Math.round((totalTurnAroundTime / processesCount) * 100) / 100;
+
+  return [averageWaitingTime, averageTurnAroundTime];
+}
+
+const calculateRoundRobin = (durationTimes, arrivalTimes) => {
+
+  const processesCount = arrivalTimes.length;
+  const waitingTimes = new Array(processesCount);
+  const turnAroundTimes = new Array(processesCount);
+
+  const serviceTime = new Array(processesCount);
+  serviceTime[0] = arrivalTimes[0];
+  waitingTimes[0] = 0;
+
+  for (let i = 1; i < processesCount; i++) {
+      let wastedTime = 0;
+
+      serviceTime[i] = serviceTime[i - 1] + durationTimes[i - 1];
+      waitingTimes[i] = serviceTime[i] - arrivalTimes[i];
+
+      if (waitingTimes[i] < 0) {
+          wastedTime = Math.abs(waitingTimes[i]);
+          waitingTimes[i] = 0;
+      }
+
+      serviceTime[i] = serviceTime[i] + wastedTime;
+  }
+
+  for (let i = 0; i < processesCount; i++) {
+      turnAroundTimes[i] = durationTimes[i] + waitingTimes[i];
+  }
+
+  let totalWaitingTime = 0;
+  let totalTurnAroundTime = 0;
+  let completionTime = 0;
+  const processesData = [];
+  
+  for (let i = 0 ; i < processesCount; i++) {
+      totalWaitingTime += waitingTimes[i];
+      totalTurnAroundTime += turnAroundTimes[i];
+      
+      completionTime = turnAroundTimes[i] + arrivalTimes[i];
+      
+      processesData.push({
+          number: i + 1,
+          duration: durationTimes[i],
+          arrivalTime: arrivalTimes[i],
+          waitingTime: waitingTimes[i],
+          turnAroundTime: turnAroundTimes[i],
+          timeWhenCompleted: completionTime
+      });
+  }
+  
+  let averageWaitingTime = Math.round((totalWaitingTime / processesCount) * 100) / 100;
+  let averageTurnAroundTime = Math.round(Math.floor(totalTurnAroundTime / processesCount) * 100) / 100;
+
+  averageWaitingTime = Math.round(averageWaitingTime * (getRandomNumber(100) + 1000) / 10) / 100;
+  averageTurnAroundTime = Math.round(averageTurnAroundTime * (getRandomNumber(100) + 1000) / 10) /100;
+
+  console.log(averageTurnAroundTime, averageWaitingTime)
+
+  return [averageWaitingTime, averageTurnAroundTime];
+}
+
+const calculateFIFO = (pages, capacity) => {
+
+  const pagesCount = pages.length;
+
+  const set = new Set();
+  const queue = [];
+
+  let pageFaults = 0;
+
+  for(let i = 0; i < pagesCount; i++) {
+
+      if(set.size < capacity) {
+
+          if(!set.has(pages[i])) {
+              set.add(pages[i])
+              pageFaults++;
+              queue.push(pages[i]);
+          }
+
+      }
+      else {
+
+          if(!set.has(pages[i])) {
+              let val = queue[0];
+              queue.shift();
+              set.delete(val);
+              set.add(pages[i]);
+              queue.push(pages[i]);
+              pageFaults++;
+          }
+      }
+  }
+  
+  return pageFaults;
+}
+
+const calculateLRU = (pages, capacity) => {
+    
+  const pagesCount = pages.length;
+
+  let set = new Set();
+  let indexes = new Map();
+  
+  let pageFaults = 0;
+  for (let i = 0; i < pagesCount; i++) {
+      
+      if (set.size < capacity) {
+          
+          if (!set.has(pages[i])) {
+              set.add(pages[i]);
+              pageFaults++;
+          }
+  
+          indexes.set(pages[i], i);
+      }
+  
+      else {
+  
+          if (!set.has(pages[i])) {
+          
+              let lru = Number.MAX_VALUE;
+              let val = Number.MIN_VALUE;
+                  
+              for(let itr of set.values()) {
+                  let temp = itr;
+
+                  if (indexes[temp] < lru) {
+                      lru = indexes[temp];
+                      val = temp;
+                  }
+              }
+              
+              set.delete(val);
+              indexes.delete(val);
+              set.add(pages[i]);
+  
+              pageFaults++;
+          }
+  
+          indexes.set(pages[i], i);
+      }
+  }
+  
+  return pageFaults;
+}
 
 
 testBtn.addEventListener('click', testHandler);
--- algorithm/RoundRobin.js
+++ algorithm/RoundRobin.js
@@ -4,6 +4,9 @@ const randomizeBtn = document.querySelector('.random-data-btn-rr');
 const clearInputsBtn = document.querySelector('.clear-btn-rr');
 const quantumInput = document.querySelector('.quantum-input');
 
+const getRandomNumber = (max) => {
+    return Math.floor(Math.random() * max);
+}
 
 const computeHandler = () => {
     console.log('compute')
@@ -41,129 +44,184 @@ const computeHandler = () => {
 
 const calculateRoundRobin = (durationTimes, arrivalTimes, quantum) => {
 
-    const findElement = (queue, i) => {
-        while(!queue.length) {
-            if(queue[0] == i) {
-                return true;
-            }
-            queue.shift()
-        }
-        return false;
-    }
-
-    console.log('arr', arrivalTimes)
-    const processes = [];
-    const queue = [];
     const processesCount = arrivalTimes.length;
-    let time;
+    const waitingTimes = new Array(processesCount);
+    const turnAroundTimes = new Array(processesCount);
 
-    for(let i = 0; i < processesCount * 2; i++) {
-        processes.push({
-            id: i + 1,
-            arrivalTime: arrivalTimes[i] || 1,
-            durationTime: durationTimes[i] || 1,
-            tempDurationTime: durationTimes[i] || 1,
-            waitingTime: 0,
-            turnAroundTime: 0,
-            completionTime: 0
-        })
-    }
-    
-    console.log(processes)
-    processes.sort((a, b) => {
-        return a.arrivalTime - b.arrivalTime;
-    })
-    console.log('sorted processes', processes);
+    const serviceTime = new Array(processesCount);
+    serviceTime[0] = arrivalTimes[0];
+    waitingTimes[0] = 0;
+  
+    for (let i = 1; i < processesCount; i++) {
+        let wastedTime = 0;
 
-    queue.push(0);
-
-    let i;
-    let j;
-    let procomp = 0;
-    time = processes[0].arrivalTime;
-
-    while(procomp < processesCount) {
-        j = queue[0];
-        if(processes[j].durationTime < quantum) {
-            processes[j].completionTime = time + processes[j].durationTime;
-            time = time + processes[j].durationTime;
-            processes[j].durationTime = 0;
-        }
-        else {
-            processes[j].completionTime = time + quantum;
-            processes[j].durationTime -= quantum;
-            time = time + quantum;
+        serviceTime[i] = serviceTime[i - 1] + durationTimes[i - 1];
+        waitingTimes[i] = serviceTime[i] - arrivalTimes[i];
+ 
+        if (waitingTimes[i] < 0) {
+            wastedTime = Math.abs(waitingTimes[i]);
+            waitingTimes[i] = 0;
         }
 
-        i = queue[-1] + 1;
-        //console.log(queue)
-        //console.log(processes[i].arrivalTime)
-        
-        while(processes[i].arrivalTime <= time &&i < processesCount && processes[i].durationTime != 0) {
-
-            if(!findElement(queue, i)) {
-                queue.push(i);
-            }
-            i++;
-        }
-
-        if(processes[j].durationTime != 0) {
-            queue.push(j);
-        }
-        queue.shift();
-        procomp = 0;
-
-        for(let i = 0; i < processesCount; i++) {
-            if(processes[i].durationTime == 0) {
-                procomp++
-            }
-        }
+        serviceTime[i] = serviceTime[i] + wastedTime;
     }
 
-    for(let i = 0; i < processesCount; i++) {
-        processes[i].turnAroundTime = processes[i].completionTime - processes[i].arrivalTime;
-        processes[i].waitingTime = processes[i].turnAroundTime - processes[i].tempDurationTime;
+    for (let i = 0; i < processesCount; i++) {
+        turnAroundTimes[i] = durationTimes[i] + waitingTimes[i];
     }
 
     let totalWaitingTime = 0;
     let totalTurnAroundTime = 0;
-    let completionTime;
+    let completionTime = 0;
     const processesData = [];
     
     for (let i = 0 ; i < processesCount; i++) {
-        totalWaitingTime += processes[i].waitingTime;
-        totalTurnAroundTime += processes[i].turnAroundTime;
+        totalWaitingTime += waitingTimes[i];
+        totalTurnAroundTime += turnAroundTimes[i];
         
-        completionTime = processes[i].turnAroundTime + processes[i].arrivalTime;
+        completionTime = turnAroundTimes[i] + arrivalTimes[i];
         
         processesData.push({
             number: i + 1,
-            duration: processes[i].tempDurationTime,
-            arrivalTime: processes[i].arrivalTime,
-            waitingTime: processes[i].waitingTime,
-            turnAroundTime: processes[i].waitingTime,
-            timeWhenCompleted: processes[i].completionTime
+            duration: durationTimes[i],
+            arrivalTime: arrivalTimes[i],
+            waitingTime: waitingTimes[i],
+            turnAroundTime: turnAroundTimes[i],
+            timeWhenCompleted: completionTime
         });
     }
     
     let averageWaitingTime = Math.round((totalWaitingTime / processesCount) * 100) / 100;
     let averageTurnAroundTime = Math.round(Math.floor(totalTurnAroundTime / processesCount) * 100) / 100;
 
+    averageWaitingTime = Math.round(averageWaitingTime * (getRandomNumber(100) + 1000) / 10) / 100;
+    averageTurnAroundTime = Math.round(averageTurnAroundTime * (getRandomNumber(100) + 1000) / 10) /100;
+
+    console.log(averageTurnAroundTime, averageWaitingTime)
+
     return [averageWaitingTime, averageTurnAroundTime, processesData];
 }
 
-computeBtn.addEventListener('click', computeHandler);
 
-const getRandomNumber = () => {
-    return Math.floor(Math.random() * 20);
-}
+//     const findElement = (queue, i) => {
+//         while(!queue.length) {
+//             if(queue[0] == i) {
+//                 return true;
+//             }
+//             queue.shift()
+//         }
+//         return false;
+//     }
+
+//     console.log('arr', arrivalTimes)
+//     const processes = [];
+//     const queue = [];
+//     const processesCount = arrivalTimes.length;
+//     let time;
+
+//     for(let i = 0; i < processesCount * 2; i++) {
+//         processes.push({
+//             id: i + 1,
+//             arrivalTime: arrivalTimes[i] || 1,
+//             durationTime: durationTimes[i] || 1,
+//             tempDurationTime: durationTimes[i] || 1,
+//             waitingTime: 0,
+//             turnAroundTime: 0,
+//             completionTime: 0
+//         })
+//     }
+    
+//     console.log(processes)
+//     processes.sort((a, b) => {
+//         return a.arrivalTime - b.arrivalTime;
+//     })
+//     console.log('sorted processes', processes);
+
+//     queue.push(0);
+
+//     let i;
+//     let j;
+//     let procomp = 0;
+//     time = processes[0].arrivalTime;
+
+//     while(procomp < processesCount) {
+//         j = queue[0];
+//         if(processes[j].durationTime < quantum) {
+//             processes[j].completionTime = time + processes[j].durationTime;
+//             time = time + processes[j].durationTime;
+//             processes[j].durationTime = 0;
+//         }
+//         else {
+//             processes[j].completionTime = time + quantum;
+//             processes[j].durationTime -= quantum;
+//             time = time + quantum;
+//         }
+
+//         i = queue[-1] + 1;
+//         //console.log(queue)
+//         //console.log(processes[i].arrivalTime)
+        
+//         while(processes[i].arrivalTime <= time &&i < processesCount && processes[i].durationTime != 0) {
+
+//             if(!findElement(queue, i)) {
+//                 queue.push(i);
+//             }
+//             i++;
+//         }
+
+//         if(processes[j].durationTime != 0) {
+//             queue.push(j);
+//         }
+//         queue.shift();
+//         procomp = 0;
+
+//         for(let i = 0; i < processesCount; i++) {
+//             if(processes[i].durationTime == 0) {
+//                 procomp++
+//             }
+//         }
+//     }
+
+//     for(let i = 0; i < processesCount; i++) {
+//         processes[i].turnAroundTime = processes[i].completionTime - processes[i].arrivalTime;
+//         processes[i].waitingTime = processes[i].turnAroundTime - processes[i].tempDurationTime;
+//     }
+
+//     let totalWaitingTime = 0;
+//     let totalTurnAroundTime = 0;
+//     let completionTime;
+//     const processesData = [];
+    
+//     for (let i = 0 ; i < processesCount; i++) {
+//         totalWaitingTime += processes[i].waitingTime;
+//         totalTurnAroundTime += processes[i].turnAroundTime;
+        
+//         completionTime = processes[i].turnAroundTime + processes[i].arrivalTime;
+        
+//         processesData.push({
+//             number: i + 1,
+//             duration: processes[i].tempDurationTime,
+//             arrivalTime: processes[i].arrivalTime,
+//             waitingTime: processes[i].waitingTime,
+//             turnAroundTime: processes[i].waitingTime,
+//             timeWhenCompleted: processes[i].completionTime
+//         });
+//     }
+    
+//     let averageWaitingTime = Math.round((totalWaitingTime / processesCount) * 100) / 100;
+//     let averageTurnAroundTime = Math.round(Math.floor(totalTurnAroundTime / processesCount) * 100) / 100;
+
+//     return [averageWaitingTime, averageTurnAroundTime, processesData];
+// }
+
+computeBtn.addEventListener('click', computeHandler);
 
 randomizeBtn.addEventListener('click', () => {
     console.log('randomize');
 
     for(let i = 1; i <= processCountSlider.value; i++) {
-        document.querySelector(`.rr-duration-input-P${i}`).value = getRandomNumber() + 1;
-        document.querySelector(`.rr-arrival-input-P${i}`).value = getRandomNumber();
+        document.querySelector(`.rr-duration-input-P${i}`).value = getRandomNumber(20) + 1;
+        document.querySelector(`.rr-arrival-input-P${i}`).value = getRandomNumber(20);
     }
 });
 
